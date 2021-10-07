1b8cf9fdac5c7cd26861acb0807a30050769d764
AlexandreRouma <alexandre.rouma@gmail.com>
2021-10-07 02:02:44 +0200

    Updated the content

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+const githubReleasesURL = 'https://api.github.com/repos/AlexandreRouma/SDRPlusPlus/releases';
+const githubNightlyURL = 'https://api.github.com/repos/AlexandreRouma/SDRPlusPlus/actions/runs?status=success&actor=AlexandreRouma&branch=master';
+
+function gotoLatestRelease() {
+    let http = new XMLHttpRequest();
+    
+    http.open("GET", githubReleasesURL);
+    http.send();
+
+    http.onreadystatechange = (e) => {
+        if (http.readyState != 4 || http.status != 200) { return; }
+        let obj = JSON.parse(http.responseText);
+        window.open(obj[0].html_url, '_blank').focus();
+    }
+}
+
+function gotoNightlyBuild() {
+    let http = new XMLHttpRequest();
+    
+    http.open("GET", githubNightlyURL);
+    http.send();
+
+    http.onreadystatechange = (e) => {
+        if (http.readyState != 4 || http.status != 200) { return; }
+        let obj = JSON.parse(http.responseText);
+        window.open(obj.workflow_runs[0].html_url, '_blank').focus();
+    }
+}
