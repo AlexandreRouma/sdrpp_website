7e11d9e99f6e92a03ae16db9d497c5f4a031d27c
AlexandreRouma <alexandre.rouma@gmail.com>
2022-11-22 11:29:10 +0100

    updated nightly link and main page

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 const githubReleasesURL = 'https://api.github.com/repos/AlexandreRouma/SDRPlusPlus/releases';
-const githubNightlyURL = 'https://api.github.com/repos/AlexandreRouma/SDRPlusPlus/actions/runs?status=success&actor=AlexandreRouma&branch=master';
+//const githubNightlyURL = 'https://api.github.com/repos/AlexandreRouma/SDRPlusPlus/actions/runs?status=success&actor=AlexandreRouma&branch=master';
+const githubNightlyURL = 'https://github.com/AlexandreRouma/SDRPlusPlus/releases/tag/nightly';
 
 function gotoLatestRelease() {
     let http = new XMLHttpRequest();
@@ -15,14 +16,15 @@ function gotoLatestRelease() {
 }
 
 function gotoNightlyBuild() {
-    let http = new XMLHttpRequest();
+    // let http = new XMLHttpRequest();
     
-    http.open("GET", githubNightlyURL);
-    http.send();
+    // http.open("GET", githubNightlyURL);
+    // http.send();
 
-    http.onreadystatechange = (e) => {
-        if (http.readyState != 4 || http.status != 200) { return; }
-        let obj = JSON.parse(http.responseText);
-        document.location = obj.workflow_runs[0].html_url;
-    }
+    // http.onreadystatechange = (e) => {
+    //     if (http.readyState != 4 || http.status != 200) { return; }
+    //     let obj = JSON.parse(http.responseText);
+    //     document.location = obj.workflow_runs[0].html_url;
+    // }
+    document.location = githubNightlyURL;
 }
