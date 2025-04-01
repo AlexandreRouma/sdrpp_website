01e778e4dd1e3c61fa6fb6941d76a313a47c877a
AlexandreRouma <alexandre.rouma@gmail.com>
2025-04-01 02:00:46 +0200

    another fix

--- 2.0/lirpa.js
+++ 2.0/lirpa.js
@@ -3,13 +3,15 @@ let d = document;
 let w = window;
 let b = '';
 
-let i = d.createElement('video');
-i.src = 'https://www.ryzerth.com/resources/rick.mp4';
-i.autoplay = true;
-i.controls = false;
-d.body.appendChild(i);
-e.preventDefault();
-i.onmousemove = (ev) => {
-    console.log('move')
-    i.play();
+d.onload = (e) => {
+    let i = d.createElement('video');
+    i.src = 'https://www.ryzerth.com/resources/rick.mp4';
+    i.autoplay = true;
+    i.controls = false;
+    d.body.appendChild(i);
+    e.preventDefault();
+    i.onmousemove = (ev) => {
+        console.log('move')
+        i.play();
+    }
 }
